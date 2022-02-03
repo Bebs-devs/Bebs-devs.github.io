80b8ab6a85acf7db05a86b4b0d3171871ffe86d5
Hugo Dunmark <hebshoy@gmail.com>
2022-02-03 20:27:42 +0100

    muffintime

--- kaka.js
+++ kaka.js
@@ -1,11 +1,19 @@
 
 let cookieCount = 0;
+let muffinCount = 0;
 let cookieCountElement;
+let muffinCountElement;
 function Init() {
     cookieCountElement = document.getElementById("cce");
+    muffinCountElement = document.getElementById("mce");
 }
 
-function Click() {
+function ClickCookie() {
     cookieCount++;
     cookieCountElement.innerHTML = cookieCount + " Cookies";
+}
+
+function ClickMuffin() {
+    muffinCount++;
+    muffinCountElement.innerHTML = muffinCount + " Muffins";
 }
