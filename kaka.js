2e36b97503ef786f7d48a3b26b7f4b0640aac606
Hugo Dunmark <hebshoy@gmail.com>
2022-02-03 21:02:47 +0100

    firstUpgrade

--- kaka.js
+++ kaka.js
@@ -3,9 +3,11 @@ let cookieCount = 0;
 let muffinCount = 0;
 let cookieCountElement;
 let muffinCountElement;
+let upgradeElement1;
 function Init() {
     cookieCountElement = document.getElementById("cce");
     muffinCountElement = document.getElementById("mce");
+    upgradeElement1 = document.getElementById("ut1");
 }
 
 function ClickCookie() {
@@ -16,4 +18,15 @@ function ClickCookie() {
 function ClickMuffin() {
     muffinCount++;
     muffinCountElement.innerHTML = muffinCount + " Muffins";
+}
+
+let up1Owned = 0;
+function Upgrade(index) {
+    if (muffinCount >= 100) 
+    {
+        muffinCount -= 100;
+        up1Owned++;
+        muffinCountElement.innerHTML = muffinCount + " Muffins";
+        upgradeElement1.innerHTML = "Cost:100 Owned:" + up1Owned;
+    }
 }
