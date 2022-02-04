43b95cf35efbd759c3311e1b30c0a2dd166d87ec
the_penguin_named_bob <you@example.com>
2022-02-04 10:43:25 +0100

    up2

--- kaka.js
+++ kaka.js
@@ -4,10 +4,12 @@ let muffinCount = 0;
 let cookieCountElement;
 let muffinCountElement;
 let upgradeElement1;
+let upgradeElement2;
 function Init() {
     cookieCountElement = document.getElementById("cce");
     muffinCountElement = document.getElementById("mce");
     upgradeElement1 = document.getElementById("ut1");
+    upgradeElement2 = document.getElementById("ut2");
 
     setInterval(() => {
         muffinCount += up1Owned;
@@ -27,12 +29,35 @@ function ClickMuffin() {
 }
 
 let up1Owned = 0;
+let up2Owned = 0;
 function Upgrade(index) {
-    if (muffinCount >= 100) 
-    {
-        muffinCount -= 100;
-        up1Owned++;
-        muffinCountElement.innerHTML = muffinCount + " Muffins";
-        upgradeElement1.innerHTML = "Cost:100 Owned:" + up1Owned;
+    switch (index) {
+        case 1:
+            if (muffinCount >= 100) 
+            {
+                muffinCount -= 100;
+                up1Owned++;
+                muffinCountElement.innerHTML = muffinCount + " Muffins";
+                upgradeElement1.innerHTML = "Cost:100 Owned:" + up1Owned;
+            }  
+            break;
+        case 2:
+            if (muffinCount >= 5000) 
+            {
+                muffinCount -= 5000;
+                up2Owned++;
+                muffinCountElement.innerHTML = muffinCount + " Muffins";
+                upgradeElement2.innerHTML = "Cost:5000 Owned:" + up2Owned;
+            }
+            break;
+        default:
+            break;
     }
+
+    function BakeMuffins(amount) 
+    {
+        muffinCount += amount;
+        muffinCountElement.innerHTML = muffinCount + " Muffins";
+    }
+    
 }
