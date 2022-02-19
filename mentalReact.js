0299bfe07eb29561d1916e7d20d5f1ed04c0f5eb
Hugo Dunmark <hebshoy@gmail.com>
2022-02-19 17:34:18 +0100

    arthbuttimerthing

--- mentalReact.js
+++ mentalReact.js
@@ -1,5 +1,5 @@
-var timeElapsed = 0;
-var startTime = Date.now();
+let timeElapsed = 0;
+let startTime = Date.now();
 function StartTimer() {
     timeElapsed = 0;
     startTime = Date.now();
@@ -7,4 +7,19 @@ function StartTimer() {
 function StopTimer() {
     timeElapsed = Date.now() - startTime;
     document.getElementById("reactTime").innerHTML = timeElapsed.toString() + "ms"; 
+}
+
+
+
+function Init() {
+    const btn = document.getElementById("startButton");
+    const box = document.getElementById("stopButton");
+    btn.addEventListener("click", () => {
+        const height = document.documentElement.clientHeight;
+        const width = document.documentElement.clientWidth;
+        let randY = Math.floor((Math.random() * height) + 1);
+        let randX = Math.floor((Math.random() * width) + 1);
+        box.style.top = randY + "px";
+        box.style.right = randX + "px";
+    });
 }
