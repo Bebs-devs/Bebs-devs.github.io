34191da810f227f5b8e218fdc61eb0094acd645f
the_penguin_named_bob <you@example.com>
2022-09-27 12:54:35 +0200

    ordnx

--- algebra.js
+++ algebra.js
@@ -3,5 +3,23 @@ function GetEquation() {
 }
 
 function GetAnswer() {
-    document.getElementById("equationAnswer").innerHTML = "e";
+    document.getElementById("equationAnswer").innerHTML = Ord(7,73);
+}
+
+function Ord(n,a) 
+{   
+    console.log(`n:${n} and a:${a}`);
+    //a**1-x-infinity mod n
+    let res = 0; 
+    let perhapsX = 1;
+    while(perhapsX < 100) 
+    {
+        res = Math.pow(a,perhapsX) % n;
+        console.log(`For x=${perhapsX} is ${res}`);
+        if (res == 1) 
+        {
+            return perhapsX;
+        }
+        perhapsX++;
+    }
 }
